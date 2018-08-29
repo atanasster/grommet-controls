648de01174d774d2a3747ae553e3d27b5868de80
atanasster <atanasster@gmail.com>
2018-08-29 10:37:54 -0400

    webpack 4

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,7 +18,7 @@ export default {
     path: path.resolve('./dist'),
     filename: 'grommet.min.js',
     libraryTarget: 'var',
-    library: 'Grommet',
+    library: 'GrommetControls',
   },
   externals: {
     'react': 'React',
