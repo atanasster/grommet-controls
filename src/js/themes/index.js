516e632f66621ebd9bd72dc2f70965f2001ebc83
atanasster <atanasster@gmail.com>
2018-08-30 03:08:18 -0400

    themes exports

--- src/js/themes/index.js
+++ src/js/themes/index.js
@@ -1,6 +1,6 @@
 export { default as black } from './black';
-export { default as materiallight } from './material-light';
-export { default as materialdark } from './material-dark';
+export { default as materiallight } from './materiallight';
+export { default as materialdark } from './materialdark';
 export { default as metro } from './metro';
 export { default as colorsFromArray } from './colorsFromArray';
 export { default as colorsFromObject } from './colorsFromObject';
