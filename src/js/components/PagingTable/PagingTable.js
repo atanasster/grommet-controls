be9cf467af67349d07e8bd2a90d08c62ae54cb63
atanasster <atanasster@gmail.com>
2018-09-17 02:04:38 -0400

    PagingTable responsive columns -responsiveShow -responsiveHide

--- src/js/components/PagingTable/PagingTable.js
+++ src/js/components/PagingTable/PagingTable.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { ResponsiveContext } from 'grommet/contexts';
 import { StyledPagingTable } from './StyledPagingTable';
 
-const PagingTable = ({ data, ...rest }) => {
+const responsiveColumns = (size, columns) => (
+  Array.isArray(columns) ?
+    columns.map((column) => {
+      let show = column.show;
+      if (column.responsiveShow) {
+        const sizes = Array.isArray(column.responsiveShow) ?
+          column.responsiveShow : [column.responsiveShow];
+        if (sizes.indexOf(size) !== -1) {
+          show = true;
+        }
+      }
+      if (column.responsiveHide) {
+        const sizes = Array.isArray(column.responsiveHide) ?
+          column.responsiveHide : [column.responsiveHide];
+        if (sizes.indexOf(size) !== -1) {
+          show = false;
+        }
+      }
+      return { ...column, show };
+    }) : columns
+);
+const PagingTable = ({ data, columns, ...rest }) => {
   const defaults = {
     showPagination: (rest.onFetchData !== undefined) ||
     (data && data.length > (rest.defaultPageSize || 20)),
@@ -9,10 +31,15 @@ const PagingTable = ({ data, ...rest }) => {
   };
   const newProps = { ...defaults, ...rest };
   return (
-    <StyledPagingTable
-      data={data || []}
-      {...newProps}
-    />
+    <ResponsiveContext.Consumer>
+      {size => (
+        <StyledPagingTable
+          data={data || []}
+          columns={responsiveColumns(size, columns)}
+          {...newProps}
+        />
+        )}
+    </ResponsiveContext.Consumer>
   );
 };
 
