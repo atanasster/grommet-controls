c3b6bf6a791d253b540e29058e299bed19d38884
atanasster <atanasster@gmail.com>
2019-10-20 10:45:31 +0300

    added google analytics to storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,6 @@
+window.STORYBOOK_GA_ID = 'UA-118001856-1';
+
 import '@storybook/addon-a11y/register';
+import '@storybook/addon-google-analytics/register';
 import 'storybook-addon-grommet/register';
 import 'storybook-addon-deps/register';
