30f89bfaa819f21b86fe05ed854a13cad8088777
atanasster <atanasster@gmail.com>
2019-10-20 11:38:42 +0300

    added grommet-controls logo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { dependenciesMap } from 'storybook-dep-webpack-plugin/runtime/main';
 import { configure, addDecorator, addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
 import { withA11y } from '@storybook/addon-a11y';
 import { withGrommet } from 'storybook-addon-grommet';
 import { grommet, dark } from 'grommet';
+import GrommetLogo from './assets/grommet-logo.svg';
 import { black, light, materialdark, materiallight, metro } from '../src/themes';
 
 addDecorator(withA11y);
@@ -27,6 +29,12 @@ addDecorator(withGrommet({
 addParameters({
   options: {
     showPanel: false,
+    theme: {
+      ...themes.light,
+      brandImage: GrommetLogo,
+      brandTitle: 'grommet-controls',
+      url: 'https://github.com/atanasster/grommet-controls.git'
+    },
   },
   dependencies: {
     mapper: dependenciesMap,
