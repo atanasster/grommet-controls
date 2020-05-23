3d9d4abe301df1b47bbc98b62c243b8e6b5fc539
atanasster <atanasster@gmail.com>
2020-05-23 02:41:18 -0400

    docs: updated storybook site

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,39 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  presets: [
-    {
-      name: 'storybook-addon-deps/preset',
-      options: {
-        maxLevels: 6,
-      },
-    },  
-    
-  ],
   stories: [
-    './*.stories.mdx',
-    './*.stories.tsx',
     '../src/components/**/*.stories.tsx',
     '../src/chartjs/**/*.stories.tsx',
   ],
-  addons: [
-    'storybook-addon-grommet',
-    {
-      name: '@storybook/preset-typescript',
-      options: {
-        tsLoaderOptions: {
-          configFile: path.resolve(__dirname, '../tsconfig.json'),
-        },
-      },
-    },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
-    '@component-controls/storybook'
-  ],
+  addons: ['storybook-addon-grommet',{
+    name: '@component-controls/storybook',
+    options: {
+      webpack: ['instrument',
+      'react-docgen-typescript'
+      ],
+    }
+  }],
   webpack: async (config, { configType }) => ({
     ...config,
     resolve: {
--- docs/main.9c8de16929f3e6f69512.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{2382:function(n,o){},523:function(n,o,p){p(524),p(672),p(1329),p(1331),p(2479),n.exports=p(2542)},589:function(n,o){}},[[523,1,2]]]);
