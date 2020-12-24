d9a9e82251d7e0b388a1bd7e9e619bbcbf619add
atanasster <atanasster@gmail.com>
2020-12-24 01:37:36 -0500

    update themes page

--- .config/ThemesPage.tsx
+++ .config/ThemesPage.tsx
@@ -1,33 +1,41 @@
 /* eslint-disable react/display-name */
 import React from 'react';
-import {
-  Story,
-  Description,
-} from '@component-controls/blocks';
+import { Story, Description } from '@component-controls/blocks';
 import { ThemeProvider } from 'styled-components';
 import { grommet, dark } from 'grommet';
-import { black, light, materialdark, materiallight, metro } from '../src/themes';
-
 import {
-  HoverBox,
-} from '@component-controls/components';
+  black,
+  light,
+  materialdark,
+  materiallight,
+  metro,
+} from '../src/themes';
+
+import { HoverBox } from '@component-controls/components';
 
 const themes = {
-  grommet, dark, black, light, materialdark, materiallight, metro
-}
+  grommet,
+  dark,
+  black,
+  light,
+  materialdark,
+  materiallight,
+  metro,
+};
 
-export const ThemesPage = () => {
+const ThemesPage = () => {
   return (
     <>
       <Description />
       {Object.keys(themes).map(themeName => (
         <HoverBox key={themeName} label={themeName}>
-        <ThemeProvider theme={themes[themeName]}>
-          <Story id="."  sxStyle={{ mb: 0 }}/>
-        </ThemeProvider>
+          <ThemeProvider theme={themes[themeName]}>
+            <Story id="." sx={{ mb: 0 }} />
+          </ThemeProvider>
         </HoverBox>
       ))}
     </>
   );
-}
+};
 
+export default ThemesPage;
